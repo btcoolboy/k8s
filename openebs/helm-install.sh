0215e7fe7791c83f984ae28ce553c066335ee941
Guofei Tan <godfrey.tan@outlook.com>
2022-09-05 16:07:02 +0800

    #fix bug

--- openebs/helm-install.sh
+++ openebs/helm-install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-helm install openebs-jiva -f ./openebs-jiva.yaml openebs-jiva/jiva --namespace openebs --create-namespace
+helm install openebs-localpv -f ./openebs-localpv.yaml openebs-localpv/localpv-provisioner --namespace openebs --create-namespace
+helm install openebs-jiva -f ./openebs-jiva.yaml openebs-jiva/jiva --namespace openebs 
