152e9c2419af5d6e4ac4e9a88dd40bd4662ede6e
Guofei Tan <godfrey.tan@outlook.com>
2022-09-05 17:12:25 +0800

    #fix again

--- openebs/helm-install.sh
+++ openebs/helm-install.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+localregistry="10.200.101.20:8083/"
 
-helm install openebs-localpv -f ./openebs-localpv.yaml openebs-localpv/localpv-provisioner --namespace openebs --create-namespace
-helm install openebs-jiva -f ./openebs-jiva.yaml openebs-jiva/jiva --namespace openebs 
+helm repo add openebs-jiva https://openebs.github.io/jiva-operator
+helm repo update
+helm install openebs-jiva openebs-jiva/jiva --namespace openebs --create-namespace \
+        --set-string localpv-provisioner.localpv.image.registry=$localregistry \
+        --set-string localpv-provisioner.helperPod.image.registry=$localregistry \
+	--set-string jivaOperator.controller.image.registry=$localregistry \
+        --set-string jivaOperator.exporter.image.registry=$localregistry \
+	--set-string jivaOperator.image.registry=$localregistry \
+	--set-string jivaOperator.replica.image.registry=$localregistry \
+	--set-string csiController.attacher.image.registry=$localregistry \
+	--set-string csiController.driverRegistrar.image.registry=$localregistry \
+	--set-string csiController.livenessprobe.image.registry=$localregistry \
+	--set-string csiController.provisioner.image.registry=$localregistry \
+	--set-string csiController.resizer.image.registry=$localregistry \
+	--set-string jivaCSIPlugin.image.registry=$localregistry \
+	--set-string csiNode.driverRegistrar.image.registry=$localregistry \
+	--set-string csiNode.livenessprobe.image.registry=$localregistry \
+	--set-string storageClass.name=openebs-jiva-csi \
+	--set-string storageClass.isDefaultClass=true \
+	--set-string defaultPolicy.name=openebs-jiva-policy \
+	--set-string defaultPolicy.replicas=3
+
+kubectl apply -f ./jiva-pvc.yaml
