5056c88b44b55bb92139db9f200856e258dfa297
Guofei Tan <godfrey.tan@outlook.com>
2022-11-11 00:24:26 +0800

    update

--- kubernetes/post-install.sh
+++ kubernetes/post-install.sh
@@ -2,6 +2,6 @@
 kubectl=`find ./kubekey/kube/ -name kubectl`
 sudo cp $kubectl /usr/local/bin/kubectl
 sudo chmod +x /usr/local/bin/kubectl
-mkdir ~/.kube
+mkdir -p ~/.kube
 cp ./kubekey/config-k8s ~/.kube/config
 chmod 600  ~/.kube/config
