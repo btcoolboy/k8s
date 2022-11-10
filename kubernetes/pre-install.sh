13c25b3fc8cda7cfe8fd0573f07a9d82d6bc8349
Guofei Tan <61384451+btcoolboy@users.noreply.github.com>
2022-11-11 00:45:03 +0800

    Update pre-install.sh

--- kubernetes/pre-install.sh
+++ kubernetes/pre-install.sh
@@ -7,11 +7,11 @@ rm -rf kubekey-*.tar.gz
 echo "Installing helm..."
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 echo "Installing k9s..."
-wget https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_x86_64.tar.gz
-tar -xvf k9s_Linux_x86_64.tar.gz k9s
-sudo cp k9s /usr/local/bin
+curl https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_x86_64.tar.gz --output k9s_Linux_x86_64.tar.gz
+tar -xf k9s_Linux_x86_64.tar.gz k9s
+sudo mv k9s /usr/local/bin
 sudo chmod +x /usr/local/bin/k9s
-rm -rf k9s*
+rm -rf k9*.tar.gz
 k9s info
 echo ""
 kk version
