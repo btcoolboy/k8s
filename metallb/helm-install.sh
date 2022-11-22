6273cb94115c83bf08085f4c0a0e3a6f307d02a0
Guofei Tan <godfrey.tan@outlook.com>
2022-11-22 22:37:19 +0800

    #update

--- metallb/helm-install.sh
+++ metallb/helm-install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
 helm repo add metallb https://metallb.github.io/metallb
 helm repo update
 helm install metallb metallb/metallb --namespace metallb --create-namespace \
