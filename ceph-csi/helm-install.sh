7e63d091ef70075a9851f5a515c5fb0a524ea81e
Guofei Tan <godfrey.tan@outlook.com>
2022-12-03 18:44:21 +0800

    update

--- ceph-csi/helm-install.sh
+++ ceph-csi/helm-install.sh
@@ -5,10 +5,12 @@ helm repo add ceph-csi https://ceph.github.io/csi-charts
 helm repo update
 helm install ceph-csi-rbd ceph-csi/ceph-csi-rbd --namespace ceph-csi --create-namespace
 
-# ConfigMap & Secret
+# ConfigMap
 kubectl apply -f ./csi-config-map.yaml
 kubectl apply -f ./csi-kms-config-map.yaml
 kubectl apply -f ./ceph-config-map.yaml
+
+# Secret
 kubectl apply -f ./csi-rbd-secret.yaml
 
 # StorageClass
