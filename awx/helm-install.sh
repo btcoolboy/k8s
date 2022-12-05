6ff3f6c2e53ef2695f17d157431382ea6d53ae92
Guofei Tan <godfrey.tan@outlook.com>
2022-12-05 22:12:29 +0800

    #update

--- awx/helm-install.sh
+++ awx/helm-install.sh
@@ -2,4 +2,3 @@
 helm repo add awx-operator https://ansible.github.io/awx-operator
 helm repo update
 helm install awx-operator awx-operator/awx-operator --namespace awx --create-namespace -f ./values.yaml
-kubectl apply -f ./secret.yaml
