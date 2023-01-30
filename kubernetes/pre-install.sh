50542ffba0db5dafdbaa3e1978de0ad6e5db7aa0
Guofei Tan <godfrey.tan@outlook.com>
2023-01-30 21:59:14 +0800

    #20230130

--- kubernetes/pre-install.sh
+++ kubernetes/pre-install.sh
@@ -7,8 +7,8 @@ rm -rf kubekey-*.tar.gz
 echo "Installing helm..."
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 echo "Installing k9s..."
-wget https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_x86_64.tar.gz
-tar -xf k9s_Linux_x86_64.tar.gz k9s
+wget https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_amd64.tar.gz
+tar -xf k9s_Linux_amd64.tar.gz k9s
 sudo mv k9s /usr/local/bin
 sudo chmod +x /usr/local/bin/k9s
 rm -rf k9*.tar.gz
